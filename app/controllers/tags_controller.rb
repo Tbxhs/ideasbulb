efc2dca07e38e41551993d50b3be67199b3c755e
danjiang <danjiang5956@gmail.com>
2012-09-26 12:04:26 +0800

    changed tag show

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
   authorize_resource
 
   def show
-    @tag = Tag.find_by_name!(params[:id])
+    @tag = @site.tags.find(params[:id])
     @ideas = @tag.ideas.paginate(:page => params[:page]).includes(:tags,:user,:topic,:comments,:solutions).where("status = ?",IDEA_STATUS_REVIEWED_SUCCESS).order(hot_sort)
     render :layout => "list"
   end
