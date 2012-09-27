133b379d80111087950172ee795915d04693fd58
danjiang <danjiang5956@gmail.com>
2012-09-27 13:19:29 +0800

    cross sub domain *.ideasbulb.com login

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Ideasbulb::Application.config.session_store :cookie_store, key: '_ideasbulb_session'
+Ideasbulb::Application.config.session_store :cookie_store, key: '_ideasbulb_session',domain: '.ideasbulb.com'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
